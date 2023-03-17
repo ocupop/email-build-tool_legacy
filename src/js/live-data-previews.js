cdc3ccd3a777861712cbc22382bbcc219ecb6e09
PAUL STROOT <paul@ocupop.com>
2023-03-17 12:21:47 -0500

    trial and error cloudcannon.set #5

--- src/js/live-data-previews.js
+++ src/js/live-data-previews.js
@@ -22,8 +22,8 @@ async function useNewPageProps(CloudCannon) {
   console.log("TITLE",latestValue.title)
 
 
-  CloudCannon.set(title, latestValue.title);
-  // CloudCannon.set('spacing.container-width', '500');
+  CloudCannon.set('title', latestValue.title);
+  CloudCannon.set('spacing.container-width', '500');
   // CloudCannon.set('spacing["gutter-width"]', '500');
   // CloudCannon.set(spacing.gutter-width-mobile, '500');
 
